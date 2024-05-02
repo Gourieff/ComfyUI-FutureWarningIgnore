a3ebba90efef24b329623e93e54e4a4d0a97dd34
Eugene Gourieff <85128026+Gourieff@users.noreply.github.com>
2024-05-03 01:38:30 +0700

    UPD: Simplify

--- 0_FutureWarningIgnore.py
+++ 0_FutureWarningIgnore.py
@@ -1,23 +1,4 @@
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-class FutureWarningIgnore:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required":
-                    {"": ("", ),}
-                }
-
-    RETURN_TYPES = ()
-    FUNCTION = "ignore"
-
-    OUTPUT_NODE = True
-
-    CATEGORY = ""
-
-    def ignore(self):
-        pass
-
-NODE_CLASS_MAPPINGS = {
-    "FutureWarningIgnore": FutureWarningIgnore,
-}
+NODE_CLASS_MAPPINGS = {}
